5411c65f19ec3a58d294157130bbdcdeca808a51
Vinson Chen <viz@Vinsons-MacBook-Pro.local>
2017-01-29 14:25:22 -0800

    make #my_none? #my_flatten, #my_zip, #my_rotate #my_join #my_reverse #my_inject #my_transpose

--- a01.rb
+++ a01.rb
@@ -41,10 +41,87 @@ class Array
   end
 
   def my_all?
-    sekf.each do |el|
+    self.each do |el|
       return false unless yield(el)
     end
     true
   end
+
+  def my_none?
+    self.each do |el|
+      return false if yield(el)
+    end
+    true
+  end
+
+  def my_flatten
+    res_arr = []
+    self.each do |el|
+      if el.is_a?(Array)
+        res_arr += el.my_flatten
+      else
+        res_arr << el
+      end
+    end
+
+    res_arr
+  end
+
+  def my_zip(*args)
+    res_arr = []
+    self.each_with_index do |el, idx|
+      el_arr = [el]
+      args.each do |arg|
+        el_arr << arg[idx]
+      end
+      res_arr << el_arr
+    end
+    res_arr
+  end
+
+  def my_rotate(num = 1)
+    res_arr = Array.new(self.length)
+    self.each_with_index do |el, idx|
+      res_arr[idx - num] = el
+    end
+    res_arr
+  end
+
+  def my_join(sep = '')
+    res_str = ''
+    self.each do |el|
+      res_str += el.to_s
+      res_str += sep unless self.last == el
+    end
+    res_str
+  end
+
+  def my_reverse
+    res_arr = []
+    self.each do |el|
+      res_arr.insert(0, el)
+    end
+    res_arr
+  end
+
+  def my_inject(acc = nil)
+    use_arr = (acc.nil? ? self[1..-1] : self)
+    acc ||= self.first
+    use_arr.each do |el|
+      acc = yield(acc, el)
+    end
+    acc
+  end
+
+  def my_transpose
+    res_arr = Array.new(self.first.length) { [] }
+    self.each do |sub_arr|
+      sub_arr.each_with_index do |el, idx2|
+        res_arr[idx2] << el
+      end
+    end
+    res_arr
+  end
+
 
 end
